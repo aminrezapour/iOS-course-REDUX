7a7587ec3fd79a1c2b68317084516476c9810c1b
Amin Rezapour <aminrezapour@gmail.com>
2016-07-07 16:44:35 -0700

    Table view
    We learned about how to create tables, and rows

---
+++ Table/Table/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by Amin Rezapour on 7/7/16.
+//  Copyright © 2016 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var cellContent = ["Amin", "Shirin", "Negin"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellContent.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        return cell
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
