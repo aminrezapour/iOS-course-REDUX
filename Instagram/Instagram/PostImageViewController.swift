861037b812b20e496e008ac3209926220a36eeca
Amin Rezapour <aminrezapour@gmail.com>
2016-08-05 00:34:04 -0700

    Add image upload

---
+++ Instagram/Instagram/PostImageViewController.swift
@@ -0,0 +1,105 @@
+//
+//  PostImageViewController.swift
+//  Instagram
+//
+//  Created by Amin Rezapour on 8/4/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class PostImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    @IBOutlet var imageToPost: UIImageView!
+    
+    @IBAction func chooseImage(sender: AnyObject) {
+        
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        imageToPost.image = image
+    }
+
+    @IBOutlet var imageCaption: UITextField!
+    
+    @IBAction func postImage(sender: AnyObject) {
+        
+        activityIndicator = UIActivityIndicatorView(frame: self.view.frame)
+        activityIndicator.backgroundColor = UIColor(white: 1.0, alpha: 0.5)
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        var post = PFObject(className: "Post")
+        post["caption"] = imageCaption.text
+        post["userId"] = PFUser.currentUser()?.objectId
+        
+        let imageData = UIImageJPEGRepresentation(imageToPost.image!, 1)
+        let imageFile = PFFile(name: "image.jpg", data: imageData!)
+        
+        post["imageFile"] = imageFile
+        
+        post.saveInBackgroundWithBlock { (success, error) -> Void in
+            
+            self.activityIndicator.stopAnimating()
+            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+
+            
+            if error == nil {
+                
+                self.showAlert("Image Posted", message: "Good job!", action: "ok")
+                
+                self.imageToPost.image = UIImage(named: "default.png")
+                self.imageCaption.text = ""
+                
+            } else {
+                
+                self.showAlert("Oops!", message: "Posting was unsuccessful", action: "ok")
+                
+            }
+            
+        }
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showAlert(title: String, message: String, action: String){
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: action, style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+
+}
