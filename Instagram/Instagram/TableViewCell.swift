38e3fda138fdbd463f412c681054070b98cfc9dc
Amin Rezapour <aminrezapour@gmail.com>
2016-08-09 18:32:56 -0700

    Add newsfeed

---
+++ Instagram/Instagram/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  Instagram
+//
+//  Created by Amin Rezapour on 8/9/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+    
+    @IBOutlet var caption: UILabel!
+    
+    
+}
