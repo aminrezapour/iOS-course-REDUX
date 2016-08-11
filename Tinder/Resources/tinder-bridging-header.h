8f02b0193342e51a33f819cb81e9e7cbda9813e4
Amin Rezapour <aminrezapour@gmail.com>
2016-08-11 11:16:42 -0700

    Add Facebook authentication

---
+++ Tinder/Resources/tinder-bridging-header.h
@@ -0,0 +1,16 @@
+//
+//  tinder-bridging-header.h
+//  tinder
+//
+//  Created by Amin Rezapour on 8/10/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+#ifndef tinder_bridging_header_h
+#define tinder_bridging_header_h
+
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+#endif /* tinder_bridging_header_h */
---
+++ Tinder/tinders/ViewController.swift
@@ -0,0 +1,60 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+import Bolts
+import FBSDKCoreKit
+import FBSDKLoginKit
+import ParseFacebookUtilsV4
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func facebookLogin(sender: AnyObject) {
+        
+        let permissions = ["public_profile"]
+        
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if let error = error {
+                
+                print(error)
+                
+            } else {
+                
+                if user != nil {
+                    print(user)
+                }
+                
+            }
+        }
+        
+        print(PFUser.currentUser()?.objectId)
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "bar"
+//        testObject.saveInBackgroundWithBlock { (success, error) -> Void in
+//            print("Object has been saved.")
+//        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
